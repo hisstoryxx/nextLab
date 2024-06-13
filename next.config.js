66e4594dd9859b174275a5dbd12d1659a9c993b4
cho <hisstoryxx@gmail.com>
2024-06-13 21:11:55 +0900

    fix: assetprefix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   images: {
     unoptimized: true,
   },
+  // 이건 github에서 호스팅할때 쓰는거라 문제 생긴듯
   // assetPrefix:
   //   process.env.NODE_ENV === "production"
   //     ? "https://Leegunhee_test_leesy.ac.kr/"
